303e5ceed1a554cb5531f7b8b836219019edc603
Paavveel <65166970+Paavveel@users.noreply.github.com>
2020-06-23 23:28:12 +0300

    Задание 8

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var searchShow = document.querySelector(".search-body");
+var search = document.querySelector(".search-caption");
+var Form = searchShow.querySelector(".search-form");
+var dateIn = searchShow.querySelector(".search-date-in");
+var dateOut = searchShow.querySelector(".search-date-out");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("in");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+search.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchShow.classList.add("search-body-show");
+
+  if (storage) {
+    dateIn.value = storage;
+    dateOut.focus();
+  } else {
+    dateOut.value.focus();
+  }
+});
+
+Form.addEventListener("submit", function (evt) {
+  if (!dateIn.value || !dateOut.value) {
+    evt.preventDefault();
+    searchShow.classList.add("search-body-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("in", dateIn.value);
+    }
+  }
+});
